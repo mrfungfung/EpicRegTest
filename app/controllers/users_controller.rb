ed1c7b9a4b2da1897acf6707894ed155018e6689
Shu Yan Chan <github@qubit.me.uk>
2010-11-08 12:34:21 +0100

    broken DB

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,12 @@ class UsersController < ApplicationController
         @title = "Sign up"
     end
     def create
+
         @user = User.new(params[:user])
+        #{:name => "fffffff", :email=> "ffff@hotmail.com",:password=> "12341234", :password_confirmation=> "12341234"})
+        #puts "123SYCHAN"
+        #puts @user.name
+        #puts " abc "
         if @user.save
             flash[:success] = "Welcome to the Epic Reg!"
             redirect_to @user
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,10 @@ require 'digest'
 class User
 	include MongoMapper::Document
 #devise :registerable, :authenticatable, :confirmable, :recoverable, :rememberable, :trackable, :validatable
+
+
 attr_accessor :password
-attr_accessible :name, :email, :password, :password_confirmation
+attr_accessible :name, :email,:password, :password_confirmation
 #validates_presence_of :name
 EmailRegex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -11,8 +13,9 @@ EmailRegex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 key :email, String, :required =>true, :with => EmailRegex, :unique => true
 key :name, String, :required =>true, :maximum => 50
 key :password, String, :required =>true
-#key :encrypted_password, String
-#key :salt, String
+key :password_confirmation, String, :required =>true
+key :encrypted_password, String
+key :salt, String
 timestamps! # This would create a "created_at" and "updated_at" keys on create
 #!!! validates_uniqueness_of :email, :case_sensitive => false
 #^  is this still unique???
@@ -23,44 +26,11 @@ timestamps! # This would create a "created_at" and "updated_at" keys on create
 validates_confirmation_of :password
 # Password validations.
 validates_length_of :password, :within => 6..40
-
-#!!!    before_save :encrypt_password
-
-    def has_password?(submitted_password)
-        encrypted_password == encrypt(submitted_password)
-    end
-    
-    def self.authenticate(email, submitted_password)
-        user = find_by_email(email)
-        return nil if user.nil?
-        return user if user.has_password?(submitted_password)
-    end
+#!!!
 
 
-    private
-    def encrypt_password
-        self.salt = make_salt
-        self.encrypted_password = encrypt(password)
-    end
-    def encrypt(string)
-        secure_hash("#{salt}#{string}")
-    end
-    def make_salt
-        secure_hash("#{Time.now.utc}#{password}")
-    end
-    def secure_hash(string)
-        Digest::SHA2.hexdigest(string)
-    end
 
-        def create
-            @user = User.new(params[:user])
-            if @user.save
-                redirect_to @user
-            else
-                @title = "Sign up"
-                render 'new'
-            end
-        end
+
 
 
 end
